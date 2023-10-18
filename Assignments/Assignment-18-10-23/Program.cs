4c0e5d4949455ea6d06ad0d86f28693ccea8d7f3
ParvathyPanayingal <148305026+ParvathyPanayingal@users.noreply.github.com>
2023-10-18 23:22:32 +0530

    Add files via upload

---
+++ Assignments/Assignment-18-10-23/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Assignments;
+/*static void Main(string[] args)
+{
+    int[] studentGrades = { 90, 85, 78, 92 };
+    Student student = new Student("abc", studentGrades);
+    double average = student.CalculateAverage();
+    Console.WriteLine(student.studentname);
+    Console.WriteLine(average);
+}*/
+
+
+Student student = new("abc",30,70,80); //-- no need to give constructor name 
+Console.WriteLine(student.studentname);
+Console.WriteLine(student.CalculateAverage());
---
+++ Assignments/Assignment-18-10-23/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    class Student
+    {
+        public string? studentname ;
+        //public int[] grades ;
+        public int grade1;
+        public int grade2;
+        public int grade3;
+
+
+        public Student(string? studentname,int grade1,int grade2,int grade3 ) //--select the variables and ctrl . the constructirs will be created.
+        {
+            //this.grades = grades;
+            this.studentname = studentname;
+            this.grade1 = grade1;
+            this.grade2 = grade2;
+            this.grade3 = grade3;
+        }
+        /*public double CalculateAverage()
+        {
+            if(grades ==null || grades.Length ==0)
+            {  return 0.0; }
+                
+        }*/
+        
+        public double CalculateAverage()
+        {
+            double average;
+            average = (grade1 + grade2 + grade3)/3;
+            return average;
+        }
+        //int total = 0;
+        //int numberOfGrades = grades.Length;
+
+        /*for (int i=0;i < numberOfGrades;i++)
+            {
+            total += grades[+];
+            }*/
+
+         
+            
+    }
+}
