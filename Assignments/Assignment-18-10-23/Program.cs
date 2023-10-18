5cc9dc9387e06b1b869d58b9bf7ebc04f381dc81
ParvathyPanayingal <148305026+ParvathyPanayingal@users.noreply.github.com>
2023-10-18 23:38:42 +0530

    Update Program.cs

--- Assignments/Assignment-18-10-23/Program.cs
+++ Assignments/Assignment-18-10-23/Program.cs
@@ -1,15 +1,6 @@
 ﻿
 using Assignments;
-/*static void Main(string[] args)
-{
-    int[] studentGrades = { 90, 85, 78, 92 };
-    Student student = new Student("abc", studentGrades);
-    double average = student.CalculateAverage();
-    Console.WriteLine(student.studentname);
-    Console.WriteLine(average);
-}*/
 
-
-Student student = new("abc",30,70,80); //-- no need to give constructor name 
+Student student = new("abc",30,70,80); 
 Console.WriteLine(student.studentname);
 Console.WriteLine(student.CalculateAverage());
