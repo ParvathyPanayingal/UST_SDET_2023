a07aeba9dccd25b15a7609fcd1c52d43c646f9a3
ParvathyPanayingal <148305026+ParvathyPanayingal@users.noreply.github.com>
2023-10-21 00:01:36 +0530

    Add files via upload

---
+++ Assignments/20-10-23/ClothingProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class ClothingProduct : Product
+    {
+        public ClothingProduct(string? size, string? productName, double price, int quantity) : base(productName, price, quantity)
+        {
+            Size = size;
+        }
+
+        public string? Size { get; set; }
+
+
+        public void DisplayClothingProduct()
+        {
+            Console.WriteLine("Size: {0}", Size);
+        }
+    }
+}
---
+++ Assignments/20-10-23/ElectronicProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class ElectronicProduct : Product
+    {
+        public ElectronicProduct(int warrantyPeriod, string? productName, double price, int quantity) : base(productName, price, quantity)
+        {
+            WarrantyPeriod = warrantyPeriod;
+        }
+
+        public int WarrantyPeriod { get; set; }
+
+        public void DisplayWarrantyPeriod()
+        {
+            Console.WriteLine("Warranty Period: {0}", WarrantyPeriod);
+        }
+
+    }
+
+
+}
