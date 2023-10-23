88e1f238325ee4bc413c4c50d368520dd7c0f3cf
ParvathyPanayingal <148305026+ParvathyPanayingal@users.noreply.github.com>
2023-10-23 19:06:26 +0530

    Add files via upload

---
+++ 18-10-23/Electricity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programs
+{
+    class Electricity
+    {
+       public int consumernumber, previousreading, currentreading;
+        public string? consumername;
+
+        public Electricity(int consumernumber, int previousreading, int currentreading, string? consumername) //--select the variables and ctrl . the constructirs will be created.
+        {
+            this.consumernumber = consumernumber;
+            this.previousreading = previousreading;
+            this.currentreading = currentreading;
+            this.consumername = consumername;
+        }
+
+        //writing constructor
+        /*public Electricity()
+        {
+          consumernumber = 12345;
+          previousreading = 9000;
+          currentreading = 9300;
+            consumername = "YY"; //--empty string is not null it is " "
+
+        }*/
+
+
+        //wriring method
+        public double CalculateBill()
+        {
+            double billamount; //--no need to assign value because billamunt is never used in the right side of the code.
+            int reading = currentreading - previousreading;
+            if (reading <= 100) 
+            {
+                billamount = reading * 2.00;
+            }
+            else if(reading <= 200 && reading >= 101)
+            {
+                billamount =(100*2) + ((reading -100)* 2.5); 
+            }
+            else if (reading <= 401 && reading >= 201)
+            {
+                billamount = (100*2) + (100*2.5) + ((reading - 200)* 3.5);
+            }
+            else
+            {
+                billamount = (100*2) + (100*2.5) + (100*3.5) + ((reading-400)  *3.5);
+            }
+            return billamount;
+        }
+    }
+}
---
+++ 18-10-23/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*Console.WriteLine("Enter 2 numbers:");
+int num1, num2, ans;
+num1=Convert.ToInt32(Console.ReadLine());
+num2=Convert.ToInt32(Console.ReadLine());
+ans = num1 + num2;
+Console.WriteLine(ans);*/
+
+/*string? s1 = "";
+string? s2 = "";
+string s3;
+s1=Console.ReadLine();
+s2=Console.ReadLine();
+s3 = s1 + " " + s2;
+Console.WriteLine(s3);
+Console.WriteLine(s1.Equals(s2));
+Console.WriteLine(s1.ToUpper());
+Console.WriteLine(s1.ToLower());
+Console.WriteLine(s2.Substring(3));
+
+string[] s = s3.Split(' ');
+for(int i = 0; i < s.Length; i++)
+    Console.WriteLine(s[i]);*/
+
+/*double x = 7.7;
+switch(x)
+{
+    case 7.1:
+        Console.WriteLine("1");
+        break;
+
+    case 7.2:
+        Console.WriteLine("2");
+        break;
+    case 7.7:
+        Console.WriteLine("3");
+        break;
+        default: Console.WriteLine("d");
+        break;
+}*/
+
+/* using Basic_Programs;
+Calculation calculation=new Calculation();
+int num1=Convert.ToInt32(Console.ReadLine());
+int num2=Convert.ToInt32(Console.ReadLine());   
+double res=calculation.add(num1,num2);
+Console.WriteLine(res); */
+
+/*using Basic_Programs;
+
+int consumernumber = Convert.ToInt32(Console.ReadLine());
+
+Electricity electricity1 = new (12345,9000,9300,"YY"); //-- no need to give constructor name 
+//double billamount = electricity.CalculateBill();--if only for displaying there is no need to declare a variable
+Console.WriteLine(electricity1.consumernumber);
+Console.WriteLine(electricity1.consumername);
+Console.WriteLine(electricity1.CalculateBill());
+Electricity electricity2 = new (23456, 9000, 10000, "YY");
+Console.WriteLine(electricity2.consumernumber);
+Console.WriteLine(electricity2.consumername);
+Console.WriteLine(electricity2.CalculateBill());*/
+
