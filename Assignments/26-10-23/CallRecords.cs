69b4460dc41d557442b30ffe99f5e48332801374
ParvathyPanayingal <148305026+ParvathyPanayingal@users.noreply.github.com>
2023-10-26 21:51:50 +0530

    Add files via upload

---
+++ Assignments/26-10-23/CallRecords.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class CallRecord
+    {
+        public int CallId { get; set; }
+
+        public long PhoneNumber { get; set; }
+        public long CallTime { get; set; }
+
+        public static List<CallRecord> Call = new List<CallRecord>()
+        {
+            new CallRecord() { CallId = 111,PhoneNumber=9990009990,CallTime=5},
+            new CallRecord() {CallId=122,PhoneNumber=8880008880,CallTime=2},
+            new CallRecord(){CallId=133,PhoneNumber=7770007770,CallTime=7}
+
+        };
+        public static void CallHistory(long phonenumber)
+        {
+            var data = Call.FindAll(x => x.PhoneNumber == phonenumber);
+            if (data.Count() > 0)
+                foreach (var x in data)
+                {
+                    Console.WriteLine("CallId:" + x.CallId);
+                    Console.WriteLine("CallTime:" + x.CallTime);
+                }
+            else
+            {
+                Console.WriteLine("No Record Found");
+            }
+
+        }
+    }
+}
---
+++ Assignments/26-10-23/CustomerData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class CustomerData
+    {
+        int customerid;
+        string? customername;
+        int phonenumber;
+        double balance;
+
+
+        public int Customerid { get => customerid; set => customerid = value; }
+        public string? Customername { get => customername; set => customername = value; }
+        public int Phonenumber { get => phonenumber; set => phonenumber = value; }
+        public double Balance { get => balance; set => balance = value; }
+
+
+
+        public static List<CustomerData> customer = new List<CustomerData>()
+             {
+                 new CustomerData() { Customerid = 111, Customername = "Parvathy", Phonenumber = 90090090, Balance = 57899 },
+                 new CustomerData() { Customerid = 122, Customername = "Aiswarya", Phonenumber = 90094444, Balance = 35499 },
+                 new CustomerData() { Customerid = 133, Customername = "Aryamol", Phonenumber = 95678744, Balance = 67889 }
+
+
+             };
+
+        public void DispCustomerData(long phonenumber)
+        {
+            var found=customer.Find(x=>x.Phonenumber== phonenumber);    
+            if(found != null)
+            {
+                Console.WriteLine(found.Customername);
+                Console.WriteLine(found.Balance);
+            }
+            else
+            {
+                Console.WriteLine("no customer found");
+            }
+        }
+
+        public void DispAllDetails()
+        {
+
+            foreach(var customer in customer)
+            {
+                Console.WriteLine($"Customer ID: {customer.Customerid} \n Customer name: {customer.Customername} \n Phone number:{phonenumber} \n Balance:{customer.Balance}" );
+            }
+        }
+
+
+}
+
+}
