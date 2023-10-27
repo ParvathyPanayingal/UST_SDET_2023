b6833aae13c6750442b69cb6ab881bac37a7ae58
ParvathyPanayingal <148305026+ParvathyPanayingal@users.noreply.github.com>
2023-10-27 18:34:33 +0530

    Add files via upload

---
+++ 27-10-23/FileOperations.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programs
+{
+    internal class FileOperations
+    {
+        public void CreateFile()
+        {
+            FileInfo fi = new FileInfo("C:\\Users\\User\\Desktop"+"\\Files\\Sample.txt");
+            using StreamWriter str=fi.CreateText();
+            Console.WriteLine("File has been created.");
+
+            str.WriteLine("hello there");
+            str.WriteLine("HI");
+            Console.WriteLine("Written");
+
+        }
+
+        public void WriteData()
+        {
+            FileStream fs = new FileStream("C:\\Users\\User\\Desktop\\Files\\Sample2.txt",
+            FileMode.CreateNew,FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            Console.WriteLine("Enter the text:");
+            string? str = Console.ReadLine();
+            sw.WriteLine(str);
+            sw.Flush();
+            sw.Close();
+            sw.Close();
+        }
+
+        public void ReadData()
+        {
+            FileStream fs = new FileStream("C:\\Users\\User\\Desktop\\Files\\Sample.txt",
+            FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadLine();
+            while (str != null)
+            {
+                Console.WriteLine(str);
+                str=sr.ReadLine();
+            }
+            
+            sr.Close();
+            fs.Close();
+        }
+
+        public void CopyMoveFile()
+        {
+            FileInfo fi1 = new FileInfo("C:\\Users\\User\\Desktop\\Files\\Sample.txt");
+            FileInfo fi2 = new FileInfo("C:\\Users\\User\\Desktop\\Files\\Sample2.txt");
+
+            fi1.CopyTo("C:\\Users\\User\\Desktop\\Files\\Temp1\\" + "Sample.txt");
+            fi2.MoveTo("C:\\Users\\User\\Desktop\\Files\\Temp2\\" + "Sample2.txt");
+
+            
+        }
+
+        public void DeleteFile()
+        {
+            FileInfo fi = new FileInfo("C:\\Users\\User\\Desktop\\Files\\Sample.txt");
+            fi.Delete();
+
+
+        }
+
+        public void FileProperties()
+        {
+            FileInfo fi = new FileInfo("C:\\Users\\User\\Desktop\\Files\\Sample2.txt");
+            fi.Delete();
+            Console.WriteLine(fi.Name);
+            Console.WriteLine(fi.CreationTime);
+            Console.WriteLine(fi.LastWriteTime);
+            Console.WriteLine(fi.Extension);
+            Console.WriteLine(fi.LastAccessTime);
+            Console.WriteLine(fi.Exists);
+
+        }
+    }
+}
---
+++ 27-10-23/Program.cs
@@ -0,0 +1,204 @@
+﻿using Basic_Programs;
+using Basic_Programs.ExceptionMessages;
+
+/*Employee employee =new (1223,"abc","IT",10000);
+Console.WriteLine("Employee id :{0} \n"+
+    "Name : {1} \n" +
+    "Dept : {2} \n" +
+    "Basic Pay : {3} ",
+    employee.Eid,
+    employee.Empname, 
+    employee.Dept, 
+    employee.Basicpay);
+Console.WriteLine("Net Pay : {0} ",employee.CalcultaeSalary());*/
+
+/*ArraySample arraySample = new ();
+ArraySample jarray = new();
+//arraySample.Onedim();
+//arraySample.Twodim();
+arraySample.Jarray();*/
+
+/*StudentsMarks marks = new();
+marks.RollNo = 32;
+marks.StudentName = "Parvathy";
+marks.City = "Kozhikode";
+marks.Mark1 = 90;
+marks.Mark2 = 85;
+marks.Mark3 = 97;
+marks.DisplayStudentDetails();
+Console.WriteLine("Total:"+marks.CalculateTotal());
+Console.WriteLine("Average:"+marks.CalculateAverage());*/
+
+/*StudentGrade grade = new();
+grade.RollNo = 32;
+grade.StudentName = "Parvathy";
+grade.City = "Kozhikode";
+grade.Mark1 = 90;
+grade.Mark2 = 85;
+grade.Mark3 = 97;
+grade.DisplayStudentDetails();
+Console.WriteLine("Total:" + grade.CalculateTotal());
+Console.WriteLine("Average:" + grade.CalculateAverage());
+Console.WriteLine("Grade:" + grade.CalculateGrade());*/
+
+/*Console.WriteLine("1.TS 2.NTS");
+
+switch (Convert.ToInt32(Console.ReadLine()))
+{
+    case 1:
+        TeachingStaff tstaff = new TeachingStaff();
+        tstaff.Staffid = 111;
+        tstaff.Name = "TS1";
+        tstaff.Department = "CS";
+        tstaff.Specializations = "O,DBMS";
+        tstaff.Semester = 4;
+        tstaff.DisplayStaffDetails();
+        tstaff.DisplayTSDetails();
+        break; 
+
+case 2 :
+    NonTeachingStaff ntstaff = new NonTeachingStaff();
+        ntstaff.Staffid = 999;
+        ntstaff.Name = "TS1";
+        ntstaff.Department = "Admin";
+        ntstaff.Responsibilities = "Attendence";
+        ntstaff.Experience = 4;
+        ntstaff.DisplayNonTSDetails();
+        ntstaff.DisplayNonTSDetails();
+    break;
+
+}*/
+//TeachingStaff ts = new TeachingStaff();
+//NonTeachingStaff nts = new NonTeachingStaff();
+
+/*
+ EV ev = new();
+ev.Vehnum = 4343;
+ev.Brand = "MARUTI";
+ev.Model = "aa";
+ev.Disp();
+Console.WriteLine( ev.setTypeForVehicle());
+
+PV pv = new();
+pv.Vehnum =2119;
+pv.Brand = "KIA";
+pv.Model = "bb";
+pv.Disp();
+Console.WriteLine(pv.setTypeForVehicle());
+*/
+
+/*Doctor doc=new Doctor();
+doc.AddNewDoctor(1234,"abc");
+doc.DisplayDoctorDetails();
+doc.ModifyDoctor(5678,"def");
+doc.DisplayDoctorDetails();
+doc.BookApp(9876,"AAA");
+doc.DelApp("AAA");*/
+
+//BankDetails bank=new BankDetails(1234,123456789L,"AA","Inactive");
+//BankDetails bank1 = new();
+/*BankDetails bank2=new (1234,123456789L,"Parvathy","Inactive");
+
+
+bank2.WelcomeMessage();
+//bank2.WelcomeMessage("Parvathy");
+Console.WriteLine("1.Custid 2.Accno 3.Name");
+int choice=Convert.ToInt32(Console.ReadLine());
+
+switch(choice)
+{
+    case 1:
+        Console.WriteLine("Custid : ");
+        bank2.GetAccountDetails(Convert.ToInt32(Console.ReadLine()));
+        break;
+
+    case 2:
+        Console.WriteLine("Accno : ");
+        bank2.GetAccountDetails(Convert.ToInt64(Console.ReadLine()));
+        break;
+
+    case 3:
+        Console.WriteLine("Name : ");
+        bank2.GetAccountDetails(Convert.ToInt64(Console.ReadLine()));
+        break;
+
+    default:Console.WriteLine("Enter between 1-3");
+        break;
+}
+bank2.ExitMessage();*/
+
+//NGC nGC =new NGC();
+//nGC.ArrayListHandling();
+//nGC.Stackhandling();
+//nGC.Queuehandling();
+//nGC.HThandling();
+//nGC.SLHandling();
+//Basic_Programs.GC gc = new();
+//gc.ListHandling();
+//gc.Stackhandling();
+//gc.Dicthandling();
+
+/*ExceptionHandling exceptionHandling = new(10,150);
+try
+{
+    exceptionHandling.NumCheck();
+}
+catch(Num1Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+try
+{
+    exceptionHandling.NumCheckOne();
+}
+catch (Num2Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}*/
+/*
+try
+{
+
+    excep.Divide();
+
+
+}
+catch (ArithmeticException ex)
+{
+    Console.WriteLine(MyExceptions.expmesslist[1]);
+    // Console.WriteLine(ex.Source); //namespace
+}
+catch (IndexOutOfRangeException ex)
+{
+    Console.WriteLine(MyExceptions.expmesslist[2]);
+}
+catch (Exception ex)  //if this is given at the top the individual exceptions will not be catched.
+{
+    Console.WriteLine(MyExceptions.expmesslist[3]);
+}
+finally
+{
+    Console.WriteLine("Done");
+}
+*/
+//excep.NumCheck();
+
+FileOperations fo= new FileOperations();
+//fo.CreateFile();
+//fo.WriteData();
+//fo.ReadData();
+//fo.CopyMoveFile();
+//fo.DeleteFile();
+fo.FileProperties();
+        
+
+
+
+
+
+
+
+
+
+
+
